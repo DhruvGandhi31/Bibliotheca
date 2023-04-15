6686388e4cb59cce59d67b110e7fd05d8510e9a3
Karan Kamath <202151071@iiitvadodara.ac.in>
2023-04-16 03:18:05 +0530

    Big Changes (8 inch - only in Africa)

--- lib/screens/HomePage/Issue a Book/issuebooks.dart
+++ lib/screens/HomePage/Issue a Book/issuebooks.dart
@@ -1,36 +1,76 @@
 import 'package:flutter/material.dart';
+import '../../searchpage.dart';
 
 class BookIssueScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFAAD9FF),
       appBar: AppBar(
-        backgroundColor: Color(0xFF303F9F),
         title: Text('Issue a Book'),
         centerTitle: true,
       ),
-      backgroundColor: const Color(0xFFAAD9FF),
-      body: Container(
-        padding: EdgeInsets.all(16.0),
-        child: GridView.count(
-          crossAxisCount: 2,
-          mainAxisSpacing: 16.0,
-          crossAxisSpacing: 16.0,
-          children: [
-            _buildGenreContainer(
-                genre: 'History', color: Colors.blue, onTap: () {}),
-            _buildGenreContainer(
-                genre: 'Encyclopedia', color: Colors.red, onTap: () {}),
-            _buildGenreContainer(
-                genre: 'Novels', color: Colors.green, onTap: () {}),
-            _buildGenreContainer(
-                genre: 'Horror', color: Colors.orange, onTap: () {}),
-            _buildGenreContainer(
-                genre: 'Autobiography', color: Colors.brown, onTap: () {}),
-            _buildGenreContainer(
-                genre: 'Biography', color: Color(0xFF4A7C59), onTap: () {})
-          ],
-        ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SearchPage()),
+                );
+              },
+              child: Container(
+                alignment: Alignment.center,
+                width: MediaQuery.of(context).size.width * 0.9,
+                height: MediaQuery.of(context).size.height * 0.07,
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(width: 7),
+                    Icon(Icons.search),
+                    SizedBox(width: 4),
+                    Text('Search...'),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(16.0),
+              child: GridView.count(
+                crossAxisCount: 2,
+                mainAxisSpacing: 16.0,
+                crossAxisSpacing: 16.0,
+                children: [
+                  _buildGenreContainer(
+                      genre: 'History', color: Colors.blue, onTap: () {}),
+                  _buildGenreContainer(
+                      genre: 'Encyclopedia', color: Colors.red, onTap: () {}),
+                  _buildGenreContainer(
+                      genre: 'Novels', color: Colors.green, onTap: () {}),
+                  _buildGenreContainer(
+                      genre: 'Horror', color: Colors.orange, onTap: () {}),
+                  _buildGenreContainer(
+                      genre: 'Autobiography',
+                      color: Colors.brown,
+                      onTap: () {}),
+                  _buildGenreContainer(
+                      genre: 'Biography',
+                      color: Color(0xFF4A7C59),
+                      onTap: () {})
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
